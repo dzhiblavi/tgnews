e10cc39f5f3a671286ef0e2af0218e52794688ae
Роман Агеев <ageevroma2000@gmail.com>
2020-05-11 20:27:20 +0300

    language added

---
+++ cxx/language/src/lang_detect/langdetect_c.h
@@ -0,0 +1,18 @@
+//
+// Created by Роман Агеев on 11.05.2020.
+//
+#ifndef __LANGDETECT_DETECTOR_C_H_
+#define __LANGDETECT_DETECTOR_C_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void langdetect_detect(char const *data, unsigned int const length, char lang[6]);
+void langdetect_detect_with_score(char const *data, unsigned int const length, char lang[6], double *score);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ cxx/language/src/lang_detect/ngram_storage.h
@@ -0,0 +1,31 @@
+//
+// Created by Роман Агеев on 11.05.2020.
+//
+#pragma once
+#include <unordered_map>
+#include <vector>
+#include <string>
+
+namespace langdetect {
+
+class NgramStorage {
+ public:
+  typedef std::vector<double> ProbList;
+  typedef std::unordered_map<std::string, ProbList> ProbMap;
+ private:
+  static std::string const ngramdata_;
+  static std::vector<std::string> const langlist_;
+  ProbMap probmap_;
+  NgramStorage();
+  NgramStorage(NgramStorage const &) = default;
+  NgramStorage &operator=(NgramStorage const &) = default;
+ public:
+  static NgramStorage &instance();
+  ProbList const &get(std::string const &s);
+  bool has(std::string const &s);
+  std::string lang_fromindex(size_t const &idx);
+  size_t langsize();
+  size_t langindex(std::string const &name);
+};
+
+}
