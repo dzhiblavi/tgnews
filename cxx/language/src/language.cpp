cdcd4b7ec7ff31f5d7890bdfa57f64e820459ab8
Ibragim Dzhiblavi <dzhiblavi@gmail.com>
2020-05-14 10:53:38 +0300

    language added

--- cxx/language/src/language.cpp
+++ cxx/language/src/language.cpp
@@ -9,11 +9,21 @@ using std::string;
 
 
 int main(int argc, char** argv) {
-    if (argc != 2) {
-        std::cerr << "Usage: language <source-dir>" << std::endl;
+    if (argc < 2) {
+        std::cerr << "Usage: language <source-dir> [languages...]" << std::endl;
         return 1;
     }
 
-    std::cout << detect(argv[1], std::set<std::string>())->to_string() << std::endl;
+    --argc; ++argv;
+    std::filesystem::path p = argv[0];
+    --argc; ++argv;
+
+    std::set<std::string> langs;
+    while (argc > 0) {
+        langs.insert(argv[0]);
+        --argc; ++argv;
+    }
+
+    std::cout << detect(p, langs)->to_string() << std::endl;
     return 0;
 }
