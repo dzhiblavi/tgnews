b20ba5590e51ad4e243f750b06e304c9c2bba19c
gleb-kov <kovg16@gmail.com>
2020-05-19 21:13:07 +0300

    namenode disk index

--- cxx/name_daemon/name_daemon.cpp
+++ cxx/name_daemon/name_daemon.cpp
@@ -10,52 +10,56 @@ std::list<std::string> name_daemon::categories = {
         "other",
 };
 
-void name_daemon::load_file(std::filesystem::path&& path) {
-    std::ifstream ifs(path);
-    std::string data((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
-    nlohmann::json js = nlohmann::json::parse(data);
+void name_daemon::load_file(std::filesystem::path &&path) {
+    if (std::filesystem::exists(path)) {
+        std::ifstream ifs(path);
+        std::string data((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
+        nlohmann::json js = nlohmann::json::parse(data);
 
-    std::list<meta> ml;
-    for (auto const& x : js) {
-        ml.push_back({});
-        ml.back().json_file = path / x["name"];
-    }
-
-    std::lock_guard<std::mutex> lg(m);
-    for (auto&& mtt : ml) {
-        mt[mtt.json_file] = std::move(mtt);
+        std::lock_guard<std::mutex> lg(m);
+        for (auto it = js.begin(); it != js.end(); it++) {
+            mt[it.key()] = {it.value()["json_file"], it.value()["time"]};
+        }
     }
 }
 
 void name_daemon::create_directories() {
-    for (auto const& s : categories) {
+    for (auto const &s : categories) {
         std::filesystem::create_directories(BASE_DAEMON_DIR / "ru/news" / s);
         std::filesystem::create_directories(BASE_DAEMON_DIR / "en/news" / s);
     }
 }
 
+bool name_daemon::compare_time(uint64_t timep) {
+    // TODO:
+    // compare timep with now()
+    return true;
+}
+
 name_daemon::name_daemon() {
     std::cerr << "DAEMON: Boot" << std::endl;
-    if (std::filesystem::is_directory(BASE_DAEMON_DIR)) {
-        std::cerr << "DAEMON: Load" << std::endl;
-        walker w(BASE_DAEMON_DIR, [&] (std::filesystem::path&& p) {
-            load_file(std::move(p));
-        });
-        w.run();
-    } else {
-        create_directories();
-    }
+    create_directories();
+    load_file(METAINFO_FILE);
     std::cerr << "DAEMON: OK" << std::endl;
 }
 
 name_daemon::~name_daemon() {
-    // ignore
+    dump();
 }
 
-bool name_daemon::compare_time(uint64_t timep) {
-    // milli -> just seconds?
-    // compare timep with now()
-    return false;
+void name_daemon::dump() {
+    std::lock_guard<std::mutex> lg(m);
+    std::ofstream fout(METAINFO_FILE);
+
+    nlohmann::json js;
+
+    for (auto const &article : mt) {
+        if (compare_time(article.second.end)) {  // to be optimized
+            js[article.first]["json_file"] = article.second.json_file.string();
+            js[article.first]["time"] = article.second.end;
+        }
+    }
+    fout << js.dump();
 }
 
 bool name_daemon::remove(std::string const &elem) {
